0267762672769785f992dc231cb63d88a303f76e
Charles Butler <chuck@vapor.io>
2019-10-08 12:52:25 -0500

    [sctl] - Release hotfix 1.0.0-rc6

--- sctl.rb
+++ sctl.rb
@@ -1,17 +1,15 @@
 class Sctl < Formula
   desc "Manage secrets on Google Cloud Platform with KMS and state files"
   homepage "https://github.com/vapor-ware/sctl"
-  version "1.0.0-rc3"
+  version "1.0.0-rc6"
   bottle :unneeded
 
-  #depends_on "google-cloud-sdk"
-
   if OS.mac?
-    url "https://github.com/vapor-ware/sctl/releases/download/1.0.0-rc5/sctl_1.0.0-rc5_Darwin_x86_64.tar.gz"
-    sha256 "a26f1e8a2fa3621affc1f3d4e9d571687a7fb6a242998984787ffcf7c45c5904"
+    url "https://github.com/vapor-ware/sctl/releases/download/1.0.0-rc6/sctl_1.0.0-rc6_Darwin_x86_64.tar.gz"
+    sha256 "773aa96f8bfb396f8c51fde874d15a4f68960866281f5a0e52c984866eb7d660"
   elsif OS.linux?
-    url "https://github.com/vapor-ware/sctl/releases/download/1.0.0-rc5/sctl_1.0.0-rc5_Linux_x86_64.tar.gz"
-    sha256 "4c2d896a1c6c0aeda1f99be7e27b8da31d5691bbb6ebd34456cd2d7c070bc824"
+    url "https://github.com/vapor-ware/sctl/releases/download/1.0.0-rc6/sctl_1.0.0-rc6_Linux_x86_64.tar.gz"
+    sha256 "e630a54036154424ec5699e0444dc9f4d9dc9ab1708539a52e13a7b58bc3ac61"
   end
 
   def install
