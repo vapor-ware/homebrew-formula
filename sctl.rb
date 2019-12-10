654cbc90586129e7b11bc52dd29a6b9dd2a4e408
Charles Butler <chuck@vapor.io>
2019-12-10 14:20:20 -0600

    [sctl] - Release sctl 1.3.1

--- sctl.rb
+++ sctl.rb
@@ -1,15 +1,15 @@
 class Sctl < Formula
   desc "Manage secrets on Google Cloud Platform with KMS and state files"
   homepage "https://github.com/vapor-ware/sctl"
-  version "1.3.0"
+  version "1.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/vapor-ware/sctl/releases/download/1.3.0/sctl_1.3.0_Darwin_x86_64.tar.gz"
-    sha256 "6dd1baa53173ffcab5014f765407da82b6bfaf00dd2faa6cfb78c6f86880a466"
+    url "https://github.com/vapor-ware/sctl/releases/download/1.3.1/sctl_1.3.1_Darwin_x86_64.tar.gz"
+    sha256 "15368f81bfe779c87d8e07356d58c1bd190e65bdf47fc171658780e37a35b81a"
   elsif OS.linux?
-    url "https://github.com/vapor-ware/sctl/releases/download/1.3.0/sctl_1.3.0_Linux_x86_64.tar.gz"
-    sha256 "6c7163931ed9f0a8818c20013f058c1d7706a482bea2e5ff5987ed6bf5da8758"
+    url "https://github.com/vapor-ware/sctl/releases/download/1.3.1/sctl_1.3.1_Linux_x86_64.tar.gz"
+    sha256 "50e34ba2d825724b321f203a1c16591656328719dd3f3b4b10e1d107c4a72403"
   end
 
   def install
